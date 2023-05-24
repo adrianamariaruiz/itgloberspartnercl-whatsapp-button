651009ec3091a517703278b0f8df12c1284c1b8a
Adriana Ruiz <adriannaruiz24@gmail.com>
2023-05-24 09:57:21 -0500

    fix: agrega handles para usar clases de forma dinamica

--- react/components/WhatsappButton/index.tsx
+++ react/components/WhatsappButton/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
-import PoropTypes from 'prop-types'
+import PropTypes from 'prop-types'
+import { useCssHandles } from 'vtex.css-handles'
 
 type Props = {
   logo: string
   phone: string
   message: string
   width: number
-  height: number
+  height: number,
 }
 
 const WhatsappButton = ({ logo, phone, message, width, height }: Props) => {
+
+  const CSS_HANDLES = ['whatsapp_btn', 'image_btn']
+  const handles = useCssHandles(CSS_HANDLES)
+
   const formatMessage = message.replace(/ /g, '%20')
   console.log('mensaje formateado', formatMessage)
 
   return (
     <>
-      <div className="fixed bottom-2 right-2 flex flexColumn">
+      <div className={`fixed bottom-2 right-2 flex flexColumn z-999 ${handles.whatsapp_btn}`}>
         <a
           href={`https://wa.me/${phone}?text=${formatMessage}`}
           target="_blank"
@@ -26,6 +31,7 @@ const WhatsappButton = ({ logo, phone, message, width, height }: Props) => {
             alt="logo de whatsapp"
             height={height}
             width={width}
+            className={`${handles.image_btn}`}
           />
         </a>
       </div>
@@ -34,11 +40,11 @@ const WhatsappButton = ({ logo, phone, message, width, height }: Props) => {
 }
 
 WhatsappButton.PoropTypes = {
-  logo: PoropTypes.string.isRequired,
-  phone: PoropTypes.string.isRequired,
-  message: PoropTypes.string,
-  height: PoropTypes.number,
-  width: PoropTypes.number,
+  logo: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+  message: PropTypes.string,
+  height: PropTypes.number,
+  width: PropTypes.number,
 }
 
 WhatsappButton.defaultProps = {
